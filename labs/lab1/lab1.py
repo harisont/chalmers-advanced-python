886058d1338b2ceb6afae027637e1f4f380e2c8a
stoszeks <simon.stoszek@hotmail.com>
2021-11-17 16:21:42 +0100

    added build_tram_lines(), but incomplete

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -1,31 +1,35 @@
 import json
-import csv 
-
-def build_tram_stops(PATH = 'labs/data/tramstops.json'):
-  try: 
-    with open(PATH) as f:
-      raw_data = json.load(f)
-  except FileNotFoundError:
-    print(f"Data base file not found on relative path: {PATH}")
-  
-  locs = {k : {'lat': v["position"][0] , 'long': v["position"][1]} for (k,v) in raw_data.items()}
-
-def build_tram_lines(PATH = 'labs/data/tramlines.tsv'):    
-  
-  
+import csv
+import re
 
 
+def build_tram_stops(PATH = '../data/tramstops.json'):
+    try:
+        with open(PATH) as f:
+            raw_data = json.load(f)
+            locs = {k: {'lat': v["position"][0], 'long': v["position"][1]} for (k, v) in raw_data.items()}
+        print(locs)
+    except FileNotFoundError:
+        print(f"Data base file not found on relative path: {PATH}")
+    # locs = {k: {'lat': v["position"][0], 'long': v["position"][1]} for (k, v) in raw_data.items()}
 
 
+def build_tram_lines(PATH = '../data/tramlines.txt'):
+    lines = {}
+    try:
+        with open(PATH, encoding='utf8') as f:
+            raw_data = []
 
+            # I don't know how to split it!!!!!
 
-
-
-
-
-
-
+            for line in f:
+                raw_data.append(line.split("  ")[0])
+            lines_dict = {}
+        print(raw_data)
+    except FileNotFoundError:
+        print(f"Data base file not found on relative path: {PATH}")
 
 
 if __name__ == "__main__":
-  build_tram_stops()
+    build_tram_stops()
+    build_tram_lines()
