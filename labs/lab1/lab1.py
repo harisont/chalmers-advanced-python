3b3ccea91db43a2f84a749930143d52d23bbd31c
johan nilsson <nilssojo@student.chalmers.se>
2021-11-17 19:30:06 +0100

    lab1.py

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -1,35 +1,32 @@
-import json
-import csv
-import re
+'''
+The task is to write:
+1. Three functions that build dictionaries, 
+2. Four functions that extract information from them, 
+3. A dialogue function that answers to queries. 
+   The dialogue function should be divided into two parts to enable more accurate testing.'''
+
+import json, csv, re
 
 
-def build_tram_stops(PATH = '../data/tramstops.json'):
+def build_tram_stops(jsonobject = '../data/tramstops.json'):
     try:
-        with open(PATH) as f:
-            raw_data = json.load(f)
-            locs = {k: {'lat': v["position"][0], 'long': v["position"][1]} for (k, v) in raw_data.items()}
-        print(locs)
+      with open(jsonobject, 'r') as f:
+        raw_data = json.load(f)
+        locs = {k: {'lat': v["position"][0], 'long': v["position"][1]} for (k, v) in raw_data.items()}
     except FileNotFoundError:
-        print(f"Data base file not found on relative path: {PATH}")
-    # locs = {k: {'lat': v["position"][0], 'long': v["position"][1]} for (k, v) in raw_data.items()}
+      print(f"Data base file not found on relative path: {jsonobject}")
 
-
-def build_tram_lines(PATH = '../data/tramlines.txt'):
-    lines = {}
+def build_tram_lines(lines = '../data/tramlines.txt'):
     try:
-        with open(PATH, encoding='utf8') as f:
-            raw_data = []
+      with open(lines, 'r', encoding='utf8',) as f:
+          
 
-            # I don't know how to split it!!!!!
+            
+          
 
-            for line in f:
-                raw_data.append(line.split("  ")[0])
-            lines_dict = {}
-        print(raw_data)
     except FileNotFoundError:
-        print(f"Data base file not found on relative path: {PATH}")
-
+      print(f"Tramline file not found on relative path: {lines}")
 
 if __name__ == "__main__":
-    build_tram_stops()
-    build_tram_lines()
+  build_tram_stops()
+  build_tram_lines()
