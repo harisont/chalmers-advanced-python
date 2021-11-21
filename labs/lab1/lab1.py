3fc9c33e14db5831b82755c1f14b09392be262a9
johan nilsson <nilssojo@student.chalmers.se>
2021-11-21 17:43:54 +0100

    lab1.py finished teh data structure functions

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -20,17 +20,36 @@ def build_tram_stops(jsonobject = '../data/tramstops.json'):
 
 """ Parses a text file and returns a dictionary """
 def parse_file(f):
-  txt = f.read()
-  keys = [re.sub(":\n$", "", l) for l in re.findall(".+:\n", txt)]
-  stops = [re.findall(".+\S+(?=\s{2,}\d+:\d+)", s) for s in re.split(".+\n\n", txt)]
-  times = [re.findall("\d+:\d+", l) for l in re.split(".+\n\n", txt)]  
-  # print(keys)
-  # print(stops)
-  # print(times)
-  lines = dict(zip(keys, stops))
-  print(lines)
   
-  return lines
+  txt = f.read()
+  
+  keys = [re.sub(":\n$", "", l) for l in re.findall(".+:\n", txt)]
+  stops = [re.findall(".+\S+(?=\s{2,}\d+:\d+)", s) for s in re.split("\n\n", txt)]
+  times = [list(map(int, re.findall("(?<=\d\d:)\d+", l))) for l in re.split("\n\n", txt)]  
+
+  lines = dict(zip(keys, stops))
+
+  travel_times = {}
+  for (l1, l2) in zip(stops, times):
+    for (s1, t1, i) in zip(l1, l2, range(len(l1))): 
+      for (s2, t2) in zip(l1[i:], l2[i:]):
+        # print(s1, s2, abs(t2-t1)) 
+        t = t2 - t1
+        if s1 not in travel_times.keys():
+          travel_times[s1] = {s2: t}
+        if s2 not in travel_times.keys():
+          travel_times[s2] = {s1: t}
+        if s2 not in travel_times[s1].keys():
+          travel_times[s1][s2] = t
+        if s1 not in travel_times[s2].keys():
+          travel_times[s2][s1] = t
+
+        if t < travel_times[s1][s2] or t < travel_times[s2][s1]:
+          travel_times[s1][s2] = t
+          travel_times[s2][s1] = t
+
+  return lines, travel_times
+
   
 
 
