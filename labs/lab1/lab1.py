a5a3a3966c3b2339cf9340de5c3563ce5dbf1494
johan nilsson <nilssojo@student.chalmers.se>
2021-11-21 17:53:53 +0100

    reformatted to fix lab pm instructions

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -1,39 +1,27 @@
-'''
-The task is to write:
-1. Three functions that build dictionaries, 
-2. Four functions that extract information from them, 
-3. A dialogue function that answers to queries. 
-   The dialogue function should be divided into two parts to enable more accurate testing.'''
-
 import json, re
 
 
-def build_tram_stops(jsonobject = '../data/tramstops.json'):
-    try:
-      with open(jsonobject, 'r') as f:
-        raw_data = json.load(f)
-        locs = {k: {'lat': v["position"][0], 'long': v["position"][1]} for (k, v) in raw_data.items()}
-      return locs
-    except FileNotFoundError:
-      print(f"Data base file not found on relative path: {jsonobject}")
+def build_tram_stops(jsonobject):
+  raw_data = json.load(jsonobject)
+  locs = {k: {'lat': v["position"][0], 'long': v["position"][1]} for (k, v) in raw_data.items()}
+  return locs
 
 
-""" Parses a text file and returns a dictionary """
-def parse_file(f):
-  
+
+def build_tram_lines(lines):
   txt = f.read()
   
   keys = [re.sub(":\n$", "", l) for l in re.findall(".+:\n", txt)]
-  stops = [re.findall(".+\S+(?=\s{2,}\d+:\d+)", s) for s in re.split("\n\n", txt)]
-  times = [list(map(int, re.findall("(?<=\d\d:)\d+", l))) for l in re.split("\n\n", txt)]  
+  stop_lists = [re.findall(".+\S+(?=\s{2,}\d+:\d+)", s) for s in re.split("\n\n", txt)]
+  time_lists = [list(map(int, re.findall("(?<=\d\d:)\d+", l))) for l in re.split("\n\n", txt)]  
 
-  lines = dict(zip(keys, stops))
+  lines = dict(zip(keys, stop_lists))
 
   travel_times = {}
-  for (l1, l2) in zip(stops, times):
-    for (s1, t1, i) in zip(l1, l2, range(len(l1))): 
-      for (s2, t2) in zip(l1[i:], l2[i:]):
-        # print(s1, s2, abs(t2-t1)) 
+  for (sl1, sl2) in zip(stop_lists, time_lists):
+    for (s1, t1, i) in zip(sl1, sl2, range(len(sl1))): 
+      for (s2, t2) in zip(sl1[i:], sl2[i:]):
+        """ This part is ugly and could surely be written better."""
         t = t2 - t1
         if s1 not in travel_times.keys():
           travel_times[s1] = {s2: t}
@@ -50,16 +38,21 @@ def parse_file(f):
 
   return lines, travel_times
 
+
+
+def build_tram_network(somefiles = ['../data/tramstops.json' ,'../data/tramlines.txt']):
+  try:
+    with open(somefiles[0], 'r') as jsonobject:
+      build_tram_stops(jsonobject)
+  except FileNotFoundError:
+    print(f"Data base file not found on relative path: {somefiles[0]}")
   
+  try:
+    with open(somefiles[1], 'r') as txtfile:
+      build_tram_lines(txtfile)
+  except FileNotFoundError:
+    print(f"Tramline file not found on relative path: {somefiles[1]}")
 
-
-def build_tram_lines(lines = '../data/tramlines.txt'):
-    try:
-      with open(lines, 'r', encoding='utf8',) as f:
-        parse_file(f)
-    except FileNotFoundError:
-      print(f"Tramline file not found on relative path: {lines}")
 
 if __name__ == "__main__":
-  build_tram_stops()
-  build_tram_lines()
+  build_tram_stops
