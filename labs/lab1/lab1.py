c6567ec399ec8b11738a94f72551a41f837fd082
johan nilsson <nilssojo@student.chalmers.se>
2021-11-22 16:47:20 +0100

    added lines_via_stop

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -13,7 +13,7 @@ def build_tram_lines(file):
   keys = [re.sub(":\n$", "", l) for l in re.findall(".+:\n", txt)]
   stop_lists = [re.findall(".+\S+(?=\s{2,}\d+:\d+)", s) for s in re.split("\n\n", txt)]
   time_lists = [list(map(int, re.findall("(?<=\d\d:)\d+", l))) for l in re.split("\n\n", txt)]  
-
+  
   lines = dict(zip(keys, stop_lists))
 
   times = {}
@@ -66,12 +66,13 @@ def build_tram_network(somefiles = ['../data/tramstops.json' ,'../data/tramlines
     print("File exists")
 
 
-""" Function will return a alphabetically sorted list of trams that pass a stop. """
+""" Function will return an alphabetically sorted list of trams that pass a stop. """
 def lines_via_stop(lines, stop):
-  raise NotImplementedError
+  return [l if stop in lines[l] else None for l in lines.keys()].sort()
+  
 
 
-""" Function will return a alphabetically sorted list of trams that go from stop A to B. """
+""" Function will return an alphabetically sorted list of trams that go from stop A to B. """
 def lines_between_stops(somedicts, stop1, stop2):
   raise NotImplementedError
 
