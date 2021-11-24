88f7fd7457e7580f90cda331b27ef9bae413c336
johan nilsson <nilssojo@student.chalmers.se>
2021-11-24 17:36:02 +0100

    ttests

--- labs/lab1/test_tramdata.py
+++ labs/lab1/test_tramdata.py
@@ -1,7 +1,6 @@
 import unittest
-# from tramdata import *
+from tramdata import *
 import json, re
-from lab1 import *
 # from hypothesis.strategies import text
 from math import acos, cos, sin, radians
 TRAM_FILE = './tramnetwork.json'
@@ -29,41 +28,51 @@ class TestTramData(unittest.TestCase):
         for A in self.stopdict.keys():
             for B in self.stopdict.keys():
                 d = distance_between_stops(self.stopdict, A, B)  # do we need to verify this function? 
-                if d > max_dist:
-                    print(f"FAILED with dist {d} > {max_dist} between stops {A, B}", end = "\n\n")
-                    return 
+                msg = f"FAILED with dist {d} > {max_dist} between stops {A, B}"
+                self.assertLess(d, max_dist, msg)
+                    
         print(f"OK: distances < {max_dist}")
-        print("")
 
     """ Tests that all the keys found in the text file are also in the database """
     def test_lines_exists(self):
         with open("../data/tramlines.txt") as f:
             
             keys_list = set(re.findall(".+(?=:\n)", f.read()))
-
-            if self.linedict.keys() != keys_list: 
-                print("FAILED: test lines exists")
+            msg = f"{keys_list.symmetric_difference(self.linedict.keys())}"
+            self.assertEqual(keys_list, self.linedict.keys(), msg)
 
     def test_line_stops_exist(self):
+        print("")
         with open("../data/tramlines.txt") as f:
             key = ""
             stop = ""
             for row in f.readlines():
                 is_key = re.search(".+(?=:\n)", row)
-                is_stop = re.search(".+(?=\s+\d\d:\d\d\n)", row)
+                is_stop = re.search(".+(?=\s+\d+:\d+\n)", row)
 
                 if is_key:
                     key = is_key.group(0).strip()
-                    if key not in self.linedict.keys():
-                        print(f"FAILED: line: {key} not in database with lines: {self.linedict.keys()}")
-                        return
+                    msg = f"FAILED: line: {key} not in database with lines: {self.linedict.keys()}"
+                    self.assertIn(key, self.linedict.keys(), msg)
                 elif is_stop:
                     stop = is_stop.group(0).strip()
-                    if stop not in self.linedict[key]:
-                        print(f"FAILED: stop: {stop} not in database for line {key} with stops: {self.linedict.values()}")
-                        return                    
+                    msg = f"FAILED: stop: {stop} not in database for line {key} with stops: {self.linedict[key]}"
+                    self.assertIn(stop, self.linedict[key], msg)
+                        
         print("OK: all line and stops in database. ")
 
+    def test_symmetric_travel_time(self):
+        for line in self.linedict.keys():
+            for s1 in self.linedict[line]:
+                for s2 in self.linedict[line]:
+                    t1 = time_between_stops(self.linedict, self.timedict, line,  s1, s2)
+                    t2 = time_between_stops(self.linedict, self.timedict, line, s2, s1)
+                    msg = f"line: {line}, time {t1} != {t2} for {s1} to {s2}"
+                    self.assertEqual(t1,t2, msg)
+
+    
+    def test_answer_query(self):
+        pass
 
 
 
