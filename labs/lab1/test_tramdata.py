cf39d661b4fb14be00bb65fa5a5e0e68dbd0d807
johan nilsson <nilssojo@student.chalmers.se>
2021-11-24 16:42:02 +0100

    test_tramdata.py

--- labs/lab1/test_tramdata.py
+++ labs/lab1/test_tramdata.py
@@ -1,6 +1,6 @@
 import unittest
 # from tramdata import *
-import json
+import json, re
 from lab1 import *
 # from hypothesis.strategies import text
 from math import acos, cos, sin, radians
@@ -18,7 +18,7 @@ class TestTramData(unittest.TestCase):
     def test_stops_exist(self):
         stopset = {stop for line in self.linedict for stop in self.linedict[line]}
         for stop in stopset:
-            self.assertIn(stop, self.stopdict, msg= stop + ' not in stopdict')
+            self.assertIn(stop, self.stopdict, msg = stop + ' not in stopdict')
 
     # add your own tests here
 
@@ -32,11 +32,41 @@ class TestTramData(unittest.TestCase):
                 if d > max_dist:
                     print(f"FAILED with dist {d} > {max_dist} between stops {A, B}", end = "\n\n")
                     return 
-        print(f"OK: distances are smaller than {max_dist}")
+        print(f"OK: distances < {max_dist}")
         print("")
 
-
+    """ Tests that all the keys found in the text file are also in the database """
     def test_lines_exists(self):
+        with open("../data/tramlines.txt") as f:
+            
+            keys_list = set(re.findall(".+(?=:\n)", f.read()))
+
+            if self.linedict.keys() != keys_list: 
+                print("FAILED: test lines exists")
+
+    def test_line_stops_exist(self):
+        with open("../data/tramlines.txt") as f:
+            key = ""
+            stop = ""
+            for row in f.readlines():
+                is_key = re.search(".+(?=:\n)", row)
+                is_stop = re.search(".+(?=\s+\d\d:\d\d\n)", row)
+
+                if is_key:
+                    key = is_key.group(0).strip()
+                    if key not in self.linedict.keys():
+                        print(f"FAILED: line: {key} not in database with lines: {self.linedict.keys()}")
+                        return
+                elif is_stop:
+                    stop = is_stop.group(0).strip()
+                    if stop not in self.linedict[key]:
+                        print(f"FAILED: stop: {stop} not in database for line {key} with stops: {self.linedict.values()}")
+                        return                    
+        print("OK: all line and stops in database. ")
+
+
+
+
 
 
 if __name__ == '__main__':
